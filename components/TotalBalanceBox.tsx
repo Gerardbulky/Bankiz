65b6eae8aada0014b47b47962facb7007efcf7ec
Gerardbulky <gerardambe@yahoo.com>
2024-08-28 13:58:50 +0200

    Added Bank account section, Current balance and current balance

---
+++ components/TotalBalanceBox.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { formatAmount } from '../lib/utils'; // Import the formatAmount function from lib/utils.ts
+
+const TotalBalanceBox = ({
+    accounts = [], totalBanks, totalCurrentBalance     
+}: TotlaBalanceBoxProps) => {
+  return (
+    <section className='total-balance'>
+        <div className='total-balance-chart'>
+            {/* DoughnutChart */}
+        </div>
+
+        <div className='flex flex-col gap-6'>
+            <h2 className='header-2'>
+                Bank Account{totalBanks > 1 && 's'} {totalBanks} 
+            </h2>
+
+            <div className='flex flex-col gap-2'>
+                <p className='total-balance-label'>
+                    Total Current Balance
+                </p>
+                
+                <p className='total-balance-amount flex-center gap-2'>
+                    {formatAmount(totalCurrentBalance)}
+                </p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default TotalBalanceBox
